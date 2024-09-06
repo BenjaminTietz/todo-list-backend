38c5a9c7ac3ee931db51bed7de6f27eea4d28ffb
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-06 13:46:34 +0200

    refator: Update Django settings for development environment
    - Enabled debugging mode by setting `DEBUG = True`.
    - Updated `ALLOWED_HOSTS` to include localhost and 127.0.0.1 for local development.
    - Configured `CORS_ALLOWED_ORIGINS` to allow requests from `http://localhost:4200` and `http://127.0.0.1:8000/login/` for cross-origin resource sharing.
    - Added `rest_framework.authtoken` to `INSTALLED_APPS` for token-based authentication support.
    - Included `CorsMiddleware` in `MIDDLEWARE` to handle CORS requests.
    - Configured `REST_FRAMEWORK` to use `BasicAuthentication` and `SessionAuthentication` for API authentication.

--- todo_list_backend/settings.py
+++ todo_list_backend/settings.py
@@ -25,8 +25,12 @@ SECRET_KEY = 'django-insecure-h$$$))8qc1=qk0=4hi)!)7)d9=ytlzn@g*of+hjrh!vjbi6nt+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+    'http://127.0.0.1:8000/login/'
+]
 
 # Application definition
 
@@ -38,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'todolist'
+    'todolist',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
