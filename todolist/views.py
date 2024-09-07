7a32dd0eb77e3e3e2629781eedda4e4d4941b559
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-07 13:46:47 +0200

    feat: added TodoItem API endpoint with serializer and route
    - Created `TodoItemView` in `views.py` to handle GET requests and return all TodoItem objects.
    - Implemented `TodoItemSerializer` in `serializer.py` to serialize the TodoItem model for API responses.
    - Added a new route `/todos/` in `urls.py` to expose the TodoItemView endpoint.
    - The endpoint retrieves all TodoItems from the database and returns them in JSON format.
    - Currently, no authentication or permissions are applied to the endpoint.

--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.views import ObtainAuthToken, APIView
 from rest_framework.authtoken.models import Token
-
+from todolist.models import TodoItem
+from todolist.serializers import TodoItemSerializer
 # Create your views here.
+
+class TodoItemView(APIView):
+    #authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = []
+
+    def get(self, request, format=None):
+        todos = TodoItem.objects.all()
+        serializer = TodoItemSerializer(todos, many=True)
+        return Response(serializer.data)
+    
+    
 class LoginView(ObtainAuthToken):
    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
